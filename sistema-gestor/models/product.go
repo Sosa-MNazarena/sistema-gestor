554ed21f3771266e0ec851887cf5882184c20b18
Sosa-Nazarena <mariana.sosa@davinci.edu.ar>
2025-07-07 19:16:18 -0300

    feat: modelo prodcuto y stock

---
+++ sistema-gestor/models/product.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID          uint           `json:"id" gorm:"primaryKey"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
+	Nombre        string  `gorm:"not null" json:"nombre"`
+	Descripcion string  `json:"descripcion"`
+	Categoria	string  `json:"categoria"`
+	Proveedor	 string  `json:"proveedor"`
+	Precio       float64 `gorm:"not null" json:"precio"`
+	Stocks    []Stock  `gorm:"foreignKey:ProductID;references:ID" json:"stocks"`
+}
---
+++ sistema-gestor/models/stock.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Stock struct {
+	ID          uint           `json:"id" gorm:"primaryKey"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
+	ProductID uint	`json:"product_id"`
+	Sucursal  string  `json:"sucursal"`
+	Cantidad  int     `json:"cantidad"`
+}
