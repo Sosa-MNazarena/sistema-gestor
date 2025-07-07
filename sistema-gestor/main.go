4b9dd4802f0fc3f46f34f8c4939cf07b681bc60f
Sosa-Nazarena <mariana.sosa@davinci.edu.ar>
2025-07-07 19:15:06 -0300

    feat: main y files go
    Configuraciones y instalaciones de dependencias

---
+++ sistema-gestor/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	controllers "sistema-gestor/controllers"
+	_ "sistema-gestor/docs"
+	"sistema-gestor/models"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// @title API de Productos
+// @version 1.0
+// @description Esta API permite gestionar productos con GORM y Gin.
+// @host localhost:8080
+// @BasePath /
+func main() {
+	dsn := "root:@tcp(localhost:3306)/sistemagestor?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+    panic("No se puede conectar a la base de datos: " + err.Error())
+}
+
+	if err := db.AutoMigrate(&models.Product{}, &models.Stock{}); err != nil {
+		panic("failed to migrate database")
+	}
+
+	router := gin.Default()
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	controllers.InitDB(db)
+	router.POST("/products", controllers.CreateProduct)
+	router.GET("/products", controllers.GetProducts)
+
+	router.Run(":8080")
+
+}
